b231f51e4061f3e562dcffacf6ab445c3fec3c8e
Matthew Loaces <73414121+Mattlo16@users.noreply.github.com>
2024-05-23 15:31:15 -0400

    Add files via upload

---
+++ LeetCode/src/Find_the_Index_of_the_First_Occurence_in_a_String.java
@@ -0,0 +1,6 @@
+public class Find_the_Index_of_the_First_Occurence_in_a_String {
+    //This beats 100% of users 💀
+    public int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+}
