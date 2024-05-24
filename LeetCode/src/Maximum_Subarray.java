c7e63476a5d161fcf9a592e115e7ba517b72afa0
Matthew Loaces <73414121+Mattlo16@users.noreply.github.com>
2024-05-23 20:03:13 -0400

    Add files via upload

---
+++ LeetCode/src/Maximum_Subarray.java
@@ -0,0 +1,19 @@
+public class Maximum_Subarray {
+    public int maxSubArray(int[] nums) {
+        int maxSum = Integer.MIN_VALUE;
+        int currentSum = 0;
+        for (int i = 0; i < nums.length; i++)
+        {
+            currentSum = currentSum + nums[i];
+            if (maxSum < currentSum)
+            {
+                maxSum = currentSum;
+            }
+            if (currentSum < 0)
+            {
+                currentSum = 0;
+            }
+        }
+        return maxSum;
+    }
+}
