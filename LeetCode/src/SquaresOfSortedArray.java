667a07c0d2450f2eacbdc7f08aeb6d486905f8fd
Matthew Loaces <73414121+Mattlo16@users.noreply.github.com>
2024-06-08 15:25:00 -0400

    Add files via upload

---
+++ LeetCode/src/SquaresOfSortedArray.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class SquaresOfSortedArray {
+    public int[] sortedSquares(int[] nums) {
+        for (int i = 0; i < nums.length; i++)
+        {
+            nums[i] = nums[i] * nums[i];
+        }
+        Arrays.sort(nums);
+        return nums;
+    }
+}
