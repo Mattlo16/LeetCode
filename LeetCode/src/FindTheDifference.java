34d073f9cdd74a92dcc1b84080b8cfd31a4d860e
Matthew Loaces <73414121+Mattlo16@users.noreply.github.com>
2024-06-09 12:56:52 -0400

    Add files via upload

---
+++ LeetCode/src/FindTheDifference.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class FindTheDifference {
+    public char findTheDifference(String s, String t) {
+        char[] sArray = s.toCharArray();
+        char[] tArray = t.toCharArray();
+        Arrays.sort(sArray);
+        Arrays.sort(tArray);
+        for (int i = 0; i < s.length(); i++)
+        {
+            if (sArray[i] != tArray[i])
+            {
+                return tArray[i];
+            }
+        }
+        return tArray[tArray.length - 1];
+    }
+}
