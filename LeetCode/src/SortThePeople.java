6c91210e13704f5db696fa33cc811610a5580fea
Matthew Loaces <73414121+Mattlo16@users.noreply.github.com>
2024-07-22 20:31:53 -0400

    Add files via upload

---
+++ LeetCode/src/SortThePeople.java
@@ -0,0 +1,39 @@
+import java.util.Arrays;
+import java.util.HashMap;-
+
+public class SortThePeople {
+    public static String[] sortPeople(String[] names, int[] heights) {
+        //O(nlongn), Tilde: 2nlogn
+        //Beats 91%
+        int arrayLength = names.length;
+        HashMap<Integer, String> hashtable = new HashMap<>();
+        for(int i = 0; i < arrayLength; i++)
+        {
+            hashtable.put(heights[i], names[i]);
+        }
+        Arrays.sort(heights);
+        int x = 0;
+        String array[] = new String[arrayLength];
+        for (int j = arrayLength - 1; j >= 0; j--)
+        {
+            array[x] = hashtable.get(heights[j]);
+            x++;
+        }
+        return array;
+    }
+
+    public static void main(String args[])
+    {
+        String[] names = {"Mary","John","Emma"};
+        int[] sizes = {180,165,170};
+        printArray(sortPeople(names, sizes));
+    }
+
+    public static void printArray(String[] array)
+    {
+        for (int i = 0; i < array.length; i++)
+        {
+            System.out.print(array[i] + " ");
+        }
+    }
+}
